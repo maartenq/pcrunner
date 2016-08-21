459ee4017f00ac7a714ad5f7f7a46e98f0f5f74f
Maarten <@twaarten>
2016-08-21 12:27:02 +0200

    clean up setup/tox

--- setup.py
+++ setup.py
@@ -5,22 +5,6 @@
 
 import sys
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def run_tests(self):
-        # Import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -88,5 +72,4 @@ setup(
     ],
     test_suite='tests',
     tests_require=test_requirements,
-    cmdclass={'test': PyTest},
 )
