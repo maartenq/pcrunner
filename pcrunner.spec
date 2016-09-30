db9cb07a9f73aea724363f9f89d9029e5cf434a7
Maarten <@twaarten>
2016-09-30 12:30:04 +0200

    Bump version: 0.3.0 → 0.3.1

--- pcrunner.spec
+++ pcrunner.spec
@@ -1,6 +1,6 @@
 %define name pcrunner
-%define version 0.3.0
-%define unmangled_version 0.3.0
+%define version 0.3.1
+%define unmangled_version 0.3.1
 %define release 1
 
 Summary: Pcrunner (Passive Check Runner)
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if sys.platform == 'win32':
 
 setup(
     name='pcrunner',
-    version='0.3.0',
+    version='0.3.1',
     description='Pcrunner (Passive Checks Runner)',
     long_description=readme + '\n\n' + history,
     author='Maarten Diemel',
