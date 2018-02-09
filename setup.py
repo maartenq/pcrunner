57aa3ff3172a9627b6e3aa522ff3a39f69370175
Maarten <@twaarten>
2018-02-09 21:32:55 +0100

    changes from issue83

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     zip_safe=False,
     keywords='pcrunner',
     classifiers=[
-        'Development Status :: 5 - Production/Stable'
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: ISC License (ISCL)',
