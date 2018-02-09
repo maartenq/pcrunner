1a971b8406075d2457245c5fe4d512c292ce8a7d
Maarten <@twaarten>
2018-02-09 20:39:31 +0100

    Changelog for upcoming release 0.3.8

--- pcrunner/__init__.py
+++ pcrunner/__init__.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim: ts=4 et sw=4 sts=4 ft=python fenc=UTF-8 ai
+# pcrunner/__init__.py
+# vim: ai et ts=4 sw=4 sts=4 ft=python fileencoding=utf-8
 
 """
 pcrunner
